753e253f463b88de2d69a275f08246ad85761cb3
Luna <git@l4.pm>
2022-07-06 21:50:19 -0300

    remove old deduce_structure

--- callisto/main.py
+++ callisto/main.py
@@ -113,54 +113,6 @@ class Schema:
             return {"type": self.type.name, "schema": self.value}
 
 
-def deduce_structure_old(json_data):
-    if isinstance(json_data, dict):
-        schema = {}
-        for key in json_data:
-            # dont leak user info lmao
-            if key in (
-                "nick",
-                "name",
-                "username",
-                "discriminator",
-                "avatar",
-                "email",
-                "bio",
-                "session_id",
-                "icon",
-                "country_code",
-                "analytics_token",
-            ):
-                schema[key] = "<REDACTED>"
-            else:
-                schema[key] = deduce_structure(json_data[key])
-        return schema
-    elif isinstance(json_data, list):
-        # assume first element contains the rest of the list
-        # TODO find a way to attach semantic information to schema, such as
-        # outliers of a schema in a certain list
-
-        # TODO if one of the fields in the value is null, we should
-        # attempt to deduce the field across other objects, and if we
-        # find a value, wrap the result in Optional<T>
-        #
-        # this requires semantic info
-        if json_data:
-            return [deduce_structure(json_data[0]), "..."]
-        else:
-            return ["???"]
-    elif isinstance(json_data, str):
-        try:
-            value = int(json_data)
-            if value > 1420070400000:
-                return "<SNOWFLAKE>"
-            return value
-        except ValueError:
-            return json_data
-    else:
-        return json_data
-
-
 UNWANTED_KEYS = (
     "nick",
     "name",
